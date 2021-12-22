a90bf1df1bd8ede6e5cefd42886af87deda41af9
Saul Ruiz <81715186+SaulRuizS@users.noreply.github.com>
2021-12-21 23:22:43 -0600

    Initial commit

---
+++ src/components/Form.vue
@@ -0,0 +1,56 @@
+<template>
+  <form v-for="movie in movies">
+    <h3>{{ movie.name }}</h3>
+
+    <button
+      type="button"
+      v-on:click="movie.quantity -= 1"
+      disabled="{ movie.quantity < 0 }"
+    >
+      -
+    </button>
+
+    {{ movie.quantity }}
+
+    <button
+      type="button"
+      v-on:click="movie.quantity += movie.available"
+      disabled="{ movie.quantity > movie.available }"
+    >
+      +
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data() {
+    return {
+      movies: [
+        {
+          name: "Avengers",
+          available: 3,
+          quantity: 0,
+        },
+        {
+          name: "Transformers",
+          available: 6,
+          quantity: 0,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
