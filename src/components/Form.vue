85b7a3397216575e1eed015b9a337aa47632c3f7
Saul Ruiz <81715186+SaulRuizS@users.noreply.github.com>
2021-12-25 22:49:11 -0600

    Movie addition

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -43,6 +43,11 @@ export default {
           available: 7,
           quantity: 0,
         },
+        {
+          name: "Ice Age",
+          available: 9,
+          quantity: 0,
+        },
       ],
     };
   },
